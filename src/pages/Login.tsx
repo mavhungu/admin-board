e45cd79f0d6c15639c61ba05526dd4ffa5b1485b
Gilbert Mavhungu <ronewagilbert3@gmail.com>
2022-02-06 21:20:51 +0200

    adding register & login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,69 @@
-import React from 'react';
+import React,{SyntheticEvent, useState} from 'react';
+import {Navigate} from "react-router-dom";
+import axios from 'axios';
+import './Login_Register.css';
 
 export default function Login() {
+  const [email,setEmail] = useState(' ');
+  const [password,setPassword] = useState(' ');
+  const [navigate, setNavigate] = useState(false);
+  
+  const handleLoginSubmit = async (e: SyntheticEvent)=>{
+    e.preventDefault();
+    if (!email || !password) return;
+    const user = { email, password };
+    const {data} = await axios.post('/login',user);
+    
+      axios.defaults.headers.common['Authorization'] = `Bearer ${data['token']}`;
+      setNavigate(true);
+
+    setEmail(' ');
+    setPassword(' ');
+    console.log('====================================');
+    console.log(data);
+    console.log('====================================');
+
+  }
+  if (navigate) {
+      return <Navigate to="/dashboard"/>;
+  }
   return (
-    <>
-      Login
-    </>
+    <div className='App-header'>
+    <form className='form' onSubmit={handleLoginSubmit}>
+        <h4>login form</h4>
+        <div className='form-row'>
+        <label htmlFor='email' className='form-label'>
+            Email
+        </label>
+        <input
+            type='email'
+            className='form-input email-input'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+        />
+        </div>
+        <div className='form-row'>
+        <label htmlFor='password' className='form-label'>
+            Password
+        </label>
+        <input
+            type='password'
+            name='password'
+            className='form-input password-input'
+            placeholder='Enter password'
+            onChange={(e) => setPassword(e.target.value)}
+        />
+        </div>
+        <button type='submit' className='btn btn-block submit-btn'>
+        submit
+        </button>
+    </form>
+    <div className='container'>
+        <a href='/register' type='button' className='btn logout-btn'>
+            Register
+        </a>
+    </div>
+</div>
   );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,81 @@
-import React from 'react';
+import React,{SyntheticEvent, useState} from 'react';
+import {Navigate,useNavigate} from "react-router-dom";
+import axios from 'axios';
+import './Login_Register.css';
 
 function Register() {
+
+  const [name, setName]= useState('');
+  const [email,setEmail] = useState(' ');
+  const [password,setPassword] = useState(' ');
+  let navigate = useNavigate();
+
+  
+  const handleSubmit = async (e: SyntheticEvent)=>{
+    e.preventDefault();
+    if (!name || !email || !password) return;
+    const user = { name, email, password };
+    const {data} = await axios.post('/registeruser',user);
+
+    setName(' ');
+    setEmail(' ');
+    setPassword(' ');
+    console.log('====================================');
+    console.log(data);
+    console.log('====================================');
+      
+    return navigate("/")
+  }
   return (
-    <div>
-      Register
-    </div>
+<div className='App-header'>
+            <form className='form' onSubmit={handleSubmit}>
+                <h4>Register form</h4>
+                <div className='form-row'>
+                <label htmlFor='name' className='form-label'>
+                    Name
+                </label>
+                <input
+                    type='text'
+                    className='form-input email-input'
+                    name='name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                </div>
+                <div className='form-row'>
+                <label htmlFor='email' className='form-label'>
+                    Email
+                </label>
+                <input
+                    type='email'
+                    className='form-input email-input'
+                    name='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                </div>
+                <div className='form-row'>
+                <label htmlFor='password' className='form-label'>
+                    Password
+                </label>
+                <input
+                    type='password'
+                    name='password'
+                    className='form-input password-input'
+                    placeholder='Enter password'
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                </div>
+                <button type='submit' className='btn btn-block submit-btn'>
+                    submit
+                </button>
+            </form>
+            <div className='container'>
+                <a href='/' type='button' className='btn logout-btn'>
+                    Login
+                </a>
+            </div>
+        </div>
     );
 }
 
